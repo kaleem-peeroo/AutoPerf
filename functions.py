0e4866818fe4e133904ae5c3d28a0f1206075797
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2023-03-11 13:49:18 +0000

    Created new index.py and functions.py

---
+++ functions.py
@@ -0,0 +1,30 @@
+import fnmatch
+import shutil
+import sys
+import os
+import json
+import paramiko
+import time
+import concurrent.futures
+import math
+import threading
+import re
+import stat
+
+from threading import Thread
+from datetime import datetime, timedelta
+from pprint import pprint
+from itertools import product
+from itertools import repeat
+from rich.console import Console
+from rich.markdown import Markdown
+from rich.prompt import Prompt
+from rich.prompt import Confirm
+from rich.progress import track
+
+# ? Uncomment for rich traceback formatting
+# from rich.traceback import install
+# install(show_locals=True)
+
+console = Console()
+
