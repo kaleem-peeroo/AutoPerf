be8c4c2aa8b8c41a899a8672451ea82c4b0268ba
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2023-04-10 18:08:07 +0100

    Added what needs to be done in monitor.py.

---
+++ monitor.py
@@ -0,0 +1,8 @@
+from functions import *
+
+"""
+1. SSH into machine.
+2. Parse the output file to find the current campaign, campaign start and expected end of campaign.
+3. Parse the progress.json file to get punctual and prolonged test stats.
+4. Analyse the files to get usable test stats.
+"""
