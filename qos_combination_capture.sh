95cb73ad4847faedd0aca3ac103194168dc47c0d
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-04-18 17:06:56 +0100

    Added participant_allocation to configs.

--- qos_combination_capture.sh
+++ qos_combination_capture.sh
@@ -1,5 +1,5 @@
-python index.py configs/qos_combination_capture_100B.json 1.5 |& tee qos_combination_capture_100B_output.txt
-# python index.py configs/qos_combination_capture_1KB.json 1.5 |& tee qos_combination_capture_1KB_output.txt
+# python index.py configs/qos_combination_capture_100B.json 1.5 |& tee qos_combination_capture_100B_output.txt
+python index.py configs/qos_combination_capture_1KB.json 1.5 |& tee qos_combination_capture_1KB_output.txt
 # python index.py configs/qos_combination_capture_32KB.json 1.5 |& tee qos_combination_capture_32KB_output.txt
 # python index.py configs/qos_combination_capture_64KB.json 1.5 |& tee qos_combination_capture_64KB_output.txt
 # python index.py configs/qos_combination_capture_128KB.json 1.5 |& tee qos_combination_capture_128KB_output.txt
