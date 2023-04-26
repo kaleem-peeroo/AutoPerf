68e01f045d3aa9436d503e1b17cc270b9e3cc683
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-04-26 18:16:44 +0100

    Separated out the .sh files for 3pi and 5pi.

---
+++ qos_combination_capture_5pi.sh
@@ -0,0 +1,13 @@
+# PS
+# python index.py configs/5Pi/qos_combination_capture_100B_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_100B_PPSS_output.txt
+python index.py configs/5Pi/qos_combination_capture_1KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_1KB_PPSS_output.txt
+python index.py configs/5Pi/qos_combination_capture_32KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_32KB_PPSS_output.txt
+python index.py configs/5Pi/qos_combination_capture_64KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_64KB_PPSS_output.txt
+python index.py configs/5Pi/qos_combination_capture_128KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_128KB_PPSS_output.txt
+
+# AA
+# python index.py configs/5Pi/qos_combination_capture_100B_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_100B_PPSS_output.txt
+# python index.py configs/5Pi/qos_combination_capture_1KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_1KB_PPSS_output.txt
+# python index.py configs/5Pi/qos_combination_capture_32KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_32KB_PPSS_output.txt
+# python index.py configs/5Pi/qos_combination_capture_64KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_64KB_PPSS_output.txt
+# python index.py configs/5Pi/qos_combination_capture_128KB_PPSS.json 1.5 -skip-confirmation |& tee qos_combination_capture_128KB_PPSS_output.txt
