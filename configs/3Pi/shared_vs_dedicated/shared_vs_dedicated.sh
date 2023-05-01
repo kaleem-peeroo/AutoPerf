e8b46979a8aba5326deed2d8474538fbcf311e74
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2023-05-01 15:34:47 +0100

    Updated shared vs dedicated config.

---
+++ configs/3Pi/shared_vs_dedicated/shared_vs_dedicated.sh
@@ -0,0 +1,8 @@
+# Shared
+python index.py configs/3Pi/shared_vs_dedicated/qos_combination_capture_shared.json 1.5 |& tee qos_combination_capture_shared_output.txt
+
+# Dedicated PS
+# python index.py configs/3Pi/shared_vs_dedicated/qos_combination_capture_dedicated_PS.json 1.5 |& tee qos_combination_capture_dedicated_PS_output.txt
+
+# Dedicated SP
+# python index.py configs/3Pi/shared_vs_dedicated/qos_combination_capture_dedicated_SP.json 1.5 |& tee qos_combination_capture_dedicated_SP_output.txt
