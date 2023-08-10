a369c3faa26523c06ebb408500b2738485bd81b8
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2023-08-10 19:50:50 +0100

    Finished implementing dataset creator

--- dataset_creator/index.py
+++ dataset_creator/index.py
@@ -16,12 +16,117 @@ warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
 
 errors = []
 
-def create_dataset(summ_dir):
+def get_total_sub_df(df):
+    sub_cols = [col for col in df.columns if "sub" in col]
+    metrics = ['total_samples', 'samples_per_sec', 'mbps', 'lost_samples', 'lost_samples_percent']
+
+    for metric in metrics:
+        metric_cols = [col for col in sub_cols if metric in col]
+        df[f"total_{metric}"] = df[metric_cols].sum(axis=1)
+
+    return df
+
+def get_metric_stat(series, stat):
+    try:
+        stat = int(stat)
+        return series.quantile(stat / 100)
+    except ValueError:
+        if "mean" in stat:
+            return series.mean()
+        elif "std" in stat:
+            return series.std()
+        elif "min" in stat:
+            return series.min()
+        elif "max" in stat:
+            return series.max()
+        else:
+            raise ValueError(f"Unknown stat: {stat}")
+
+def get_metric_stats(df, desired_stats):
+    metric_stats = {}
+
+    total_cols = [col for col in df.columns if "total" in col and "sub" not in col]
+    latency_cols = [col for col in df.columns if "latency" in col]
+    desired_cols = total_cols + latency_cols
+
+    for col in desired_cols:
+        for stat in desired_stats:
+            metric_stats[f"{col}_{stat}"] = get_metric_stat(df[col], stat)
+    
+    for key, value in metric_stats.items():
+        if not isinstance(value, (float, int, np.int64)):
+            errors.append(f"Value of {key} is not a float or int: {value}. It is a {type(value)}")
+            raise ValueError(f"Value of {key} is not a float or int: {value}. It is a {type(value)}")
+
+    return metric_stats
+
+def get_setting_value_from_filename(filename):
+    filename = filename.replace(".csv", "")
+
+    filename_parts = filename.split("_")
+
+    sec_part = [part for part in filename_parts if "SEC" in part][0]
+    duration_secs = int(sec_part.replace("SEC", ""))
+    datalen_part = [part for part in filename_parts if "B" in part][0]
+    datalen_bytes = int(datalen_part.replace("B", ""))
+    pub_count = int([part for part in filename_parts if "P" in part][0].replace("P", ""))
+    sub_count = int([part for part in filename_parts if "S" in part and "SEC" not in part][0].replace("S", ""))
+    durability_part = int([part for part in filename_parts if "DUR" in part][0].replace("DUR", ""))
+    latency_count = int([part for part in filename_parts if "LC" in part][0].replace("LC", ""))
+    
+    if "REL" in filename:
+        reliable = True
+    else:
+        reliable = False
+    if "MC" in filename:
+        multicast = True
+    else:
+        multicast = False
+
+    return duration_secs, datalen_bytes, pub_count, sub_count, reliable, multicast, durability_part, latency_count
+
+assert get_setting_value_from_filename('600SEC_1000B_5P_1S_REL_MC_0DUR_100LC.csv') == (600, 1000, 5, 1, True, True, 0, 100)
+
+def create_dataset_from_summaries(summ_dir):
     test_csvs = os.listdir(summ_dir)
 
-    pprint(test_csvs)
+    dataset_df = pd.DataFrame()
 
-    asdf
+    for test_csv in track(test_csvs, description="Creating dataset..."):
+        test_df = pd.read_csv(os.path.join(summ_dir, test_csv), index_col=0)
+        test_df = get_total_sub_df(test_df)
+
+        duration_secs, datalen_bytes, pub_count, sub_count, reliable, multicast, durability, latency_count = get_setting_value_from_filename(test_csv)
+        
+        desired_stats = ['mean', 'std', 'min', 'max', '1', '2', '5', '10', '20', '25', '30', '40', '50', '60', '70', '75', '80', '90', '95', '99']
+
+        metric_stats = get_metric_stats(test_df, desired_stats)
+        metric_stats['duration_secs'] = duration_secs
+        metric_stats['datalen_bytes'] = datalen_bytes
+        metric_stats['pub_count'] = pub_count
+        metric_stats['sub_count'] = sub_count
+        metric_stats['reliable'] = reliable
+        metric_stats['multicast'] = multicast
+        metric_stats['durability'] = durability
+        metric_stats['latency_count'] = latency_count
+
+        dataset_df = pd.concat([dataset_df, pd.DataFrame(metric_stats, index=[0])], ignore_index=True)
+
+    dataset_df['reliable'] = dataset_df['reliable'].astype(int)
+    dataset_df['multicast'] = dataset_df['multicast'].astype(int)
+    dataset_df = pd.get_dummies(dataset_df, columns=['durability'], prefix=['durability'])
+
+    # ? Durability columns are booleans, convert them to ints
+    durability_cols = [col for col in dataset_df.columns if "durability_" in col]
+    for col in durability_cols:
+        dataset_df[col] = dataset_df[col].astype(int)
+
+    desired_columns = ['duration_secs', 'datalen_bytes', 'pub_count', 'sub_count', 'reliable', 'multicast', 'durability_0', 'durability_1', 'durability_2', 'durability_3', 'latency_count']
+    other_columns = [col for col in dataset_df.columns if col not in desired_columns]
+
+    dataset_df = dataset_df[desired_columns + other_columns]
+    
+    dataset_df.to_csv("dataset.csv", index=False)
 
 def get_all_sub_metric(sub_files):
     test_df = pd.DataFrame()
@@ -258,7 +363,7 @@ if __name__ == "__main__":
     
     summ_dir = summarise_tests(usable_tests, camp_name)
 
-    create_dataset(summ_dir)
+    create_dataset_from_summaries(summ_dir)
 
     if len(errors) > 0:
         with open("error_log.txt", "w") as file:
