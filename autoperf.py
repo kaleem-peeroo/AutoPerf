25a312148c09569cc99e134e2d51a83e10b58b4e
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-05-17 18:59:34 +0100

    Moved sys args handling to main() and implemented list comparison functions

--- autoperf.py
+++ autoperf.py
@@ -5,6 +5,7 @@ import os
 import logging
 import json
 from icecream import ic
+from typing import List
 
 # Set up logging
 logging.basicConfig(
@@ -24,22 +25,6 @@ console_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 
-SYS_ARGS = sys.argv
-
-if len(SYS_ARGS) < 2:
-    logger.error(
-        f"Config filepath not specified."
-    )
-    sys.exit(0)
-
-CONFIG_PATH = SYS_ARGS[1]
-
-if not os.path.exists(CONFIG_PATH):
-    logger.error(
-        f"Config path {CONFIG_PATH} does NOT exist."
-    )
-    sys.exit(0)
-
 def read_config(config_path: str = ""):
     if config_path == "":
         logger.error(
@@ -52,13 +37,117 @@ def read_config(config_path: str = ""):
             config = json.load(f)
         except ValueError:
             logger.error(
-                f"Error parsing JSON for config file: {CONFIG_PATH}"
+                f"Error parsing JSON for config file: {config_path}"
             )
             return None
 
+    if not isinstance(config, list):
+        logger.error(
+            f"Config file does not contain a list: {config_path}"
+        )
+        return None
+
+    REQUIRED_KEYS = [
+        'experiment_name',
+        'combination_generation_type',
+        'resuming_test_name',
+        'qos_settings',
+        'slave_machines'
+    ]
+
+    for experiment in config:
+        ic(experiment)
+        ic(experiment.keys())
+        ic(experiment.keys() == REQUIRED_KEYS)
+
     return config
 
-def main(sys_args: [str] = None) -> None:
+def get_difference_between_lists(list_one: List = [], list_two: List = []):
+    if list_one is None:
+        logger.error(
+            f"List one is none."
+        )
+        return None
+
+    if list_two is None:
+        logger.error(
+            f"List two is none."
+        )
+        return None
+
+    longer_list = get_longer_list(
+        list_one, 
+        list_two
+    )
+    if longer_list is None:
+        logger.error(
+            f"Couldn't get longer list"
+        )
+        return None
+
+    shorter_list = get_shorter_list(
+        list_one, 
+        list_two
+    )
+    if shorter_list is None:
+        logger.error(
+            f"Couldn't get shorter list"
+        )
+        return None
+
+    return [item for item in longer_list if item not in shorter_list]
+
+def get_longer_list(list_one: List = [], list_two: List = []):
+    if list_one is None:
+        logger.error(
+            f"List one is none."
+        )
+        return None
+
+    if list_two is None:
+        logger.error(
+            f"List two is none."
+        )
+        return None
+
+    if len(list_one) > len(list_two):
+        return list_one
+    else:
+        return list_two
+
+def get_shorter_list(list_one: List = [], list_two: List = []):
+    if list_one is None:
+        logger.error(
+            f"List one is none."
+        )
+        return None
+
+    if list_two is None:
+        logger.error(
+            f"List two is none."
+        )
+        return None
+
+    if len(list_one) > len(list_two):
+        return list_two
+    else:
+        return list_one
+
+def main(sys_args: list[str] = []) -> None:
+    if len(sys_args) < 2:
+        logger.error(
+            f"Config filepath not specified."
+        )
+        return None
+
+    CONFIG_PATH = sys_args[1]
+
+    if not os.path.exists(CONFIG_PATH):
+        logger.error(
+            f"Config path {CONFIG_PATH} does NOT exist."
+        )
+        return None
+
     CONFIG = read_config(CONFIG_PATH)
     if CONFIG is None:
         logger.error(
