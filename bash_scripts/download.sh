018044f1c42cea4d718dd77f273305712ec3db9c
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-09-20 19:16:32 +0100

    Updated configs in bash scripts

--- bash_scripts/download.sh
+++ bash_scripts/download.sh
@@ -1,3 +1,3 @@
 cd ~/AutoPerf;
 source .venv/bin/activate;
-python3 autoperf_results_downloader.py configs/monitor_config.json;
+python3 autoperf_full_results_downloader.py configs/downloader_config.json;
--- bash_scripts/logs.sh
+++ bash_scripts/logs.sh
@@ -1,3 +1,3 @@
 cd ~/AutoPerf;
 source .venv/bin/activate;
-python autoperf_logs_downloader.py configs/monitor_config.json
+python autoperf_logs_downloader.py configs/5pi_monitor_config.json
