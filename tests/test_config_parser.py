42d83f251839ea3b2813ff5f116cb44d64e200d6
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-11-14 16:48:09 +0000

    Added unit tests for validating test configs

--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -13,5 +13,22 @@ class TestConfigParser(unittest.TestCase):
         parser.parse()
         self.assertEqual(len(parser.config), 1)
 
+        parser = ConfigParser("config/test_configs/test_four.toml")
+        self.assertEqual(parser.filename, "config/test_configs/test_four.toml")
+        self.assertRaises(ValueError, parser.validate)
+
+        parser = ConfigParser("config/test_configs/test_five.toml")
+        self.assertEqual(parser.filename, "config/test_configs/test_five.toml")
+        self.assertRaises(ValueError, parser.validate)
+
+        parser = ConfigParser("config/test_configs/test_six.toml")
+        self.assertEqual(parser.filename, "config/test_configs/test_six.toml")
+        self.assertRaises(ValueError, parser.validate)
+
+        parser = ConfigParser("config/test_configs/test_seven.toml")
+        self.assertEqual(parser.filename, "config/test_configs/test_seven.toml")
+        self.assertRaises(ValueError, parser.validate)
+
+
 if __name__ == "__main__":
     unittest.main()
