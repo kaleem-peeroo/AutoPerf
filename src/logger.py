b7a3086a47232bcbc67b4f518eb7f9411f171eac
Kaleem Peeroo <Kaleem.Peeroo@city.ac.uk>
2024-11-19 17:34:11 +0000

    Fixed duplicate output for logger

--- src/logger.py
+++ src/logger.py
@@ -5,17 +5,14 @@ from rich.logging import RichHandler
 
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
-
 log_file_path = os.path.join(log_dir, "app.log")
 
-log_format = "%(asctime)s %(levelname)s %(message)s"
-log_level = os.getenv("LOG_LEVEL", "INFO").upper()
+file_log_format = "%(asctime)s %(levelname)s %(message)s"
+file_date_format = "[ %Y-%m-%d %H:%M:%S ]"
+log_level = logging.DEBUG
 
-logging.basicConfig(
-    level=log_level,
-    format=log_format,
-    datefmt="[ %Y-%m-%d %H:%M:%S ]",
-)
+logger = logging.getLogger("autoperf_logger")
+logger.setLevel(log_level)
 
 console_handler = RichHandler()
 console_handler.setLevel(log_level)
@@ -23,9 +20,7 @@ console_handler.setFormatter(logging.Formatter("%(message)s"))
 
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel(log_level)
-file_handler.setFormatter(logging.Formatter(log_format))
+file_handler.setFormatter(logging.Formatter(file_log_format, datefmt=file_date_format))
 
-logger = logging.getLogger("autoperf_logger")
-logger.setLevel(log_level)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
