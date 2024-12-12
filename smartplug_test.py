bbacdddf7555870608bdc3075fd1eecca6c8ad68
Kaleem Peeroo <kaleem.peeroo@city.ac.uk>
2024-12-12 08:45:28 +0000

    Added all plugs to the smart plug test script

--- smartplug_test.py
+++ smartplug_test.py
@@ -5,15 +5,26 @@ from my_secrets import TAPO_USERNAME, TAPO_PASSWORD
 async def restart():
     client = ApiClient(TAPO_USERNAME, TAPO_PASSWORD)
 
-    device = await client.p100("192.168.1.31")
+    plugs = [
+        {"name": "3p1", "ip": "192.168.1.31"},
+        {"name": "3p2", "ip": "192.168.1.31"},
 
-    logger.debug(f"Turning off {self.name} smart plug")
-    await device.off()
+        {"name": "5k2", "ip": "192.168.1.52"},
+        {"name": "5k3", "ip": "192.168.1.53"},
+        {"name": "5k4", "ip": "192.168.1.54"},
+        {"name": "5k5", "ip": "192.168.1.55"},
+    ]
 
-    logger.debug("Waiting 3 seconds...")
-    await asyncio.sleep(3)
+    for plug in plugs:
+        device = await client.p100(plug['ip'])
 
-    logger.debug(f"Turning on {self.name} smart plug")
-    await device.on()
+        print(f"Turning off {plug['name']} smart plug")
+        await device.off()
+
+        print("Waiting 3 seconds...")
+        await asyncio.sleep(3)
+
+        print(f"Turning on {plug['name']} smart plug")
+        await device.on()
 
 asyncio.run(restart())
